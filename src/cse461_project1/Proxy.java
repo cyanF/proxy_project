cc9074ec92e637e410ac3ad92f33f6f19307e19e
Qingyi Fan <fqyanna@gmail.com>
2015-05-07 21:53:25 -0700

    not connect

--- src/cse461_project1/Proxy.java
+++ src/cse461_project1/Proxy.java
@@ -1,9 +1,14 @@
 package cse461_project1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,7 +23,7 @@ public class Proxy {
 //	
 //		int port = Integer.valueOf(args[0]).intValue(); 
 //		
-		int port = 2008;
+		int port = 2011;
 		ServerSocket serverSocket;
 		try {
 			serverSocket = new ServerSocket(port);
@@ -41,23 +46,24 @@ public class Proxy {
 			this.s = s;
 		}
 		
-		public void run(){	
-			InputStream in;
+		public void run(){		
 			try {
+				InputStream in;
 				in = s.getInputStream();
 				BufferedReader br = new BufferedReader(new InputStreamReader(in));
 				int lineCount = 0;
 				int port = 80; // default
-				String request = "";
+				String requestType = "";
 				String host = "";
 				String line = "";
-				
-				while(!(line = br.readLine()).equals("")){
+				String request = "";
+				while((line = br.readLine()) != null && !line.equals("")){
 					lineCount++;
 					System.out.println(line);
 					if(lineCount == 1) {
+						request += line.replace("HTTP/1.1", "HTTP/1.0") + "\r\n";
 						System.out.println(">>> " + line);
-						request = line.split(" ")[0];
+						requestType = line.split(" ")[0];
 						String url = line.split(" ")[1];
 						if(url.toLowerCase().startsWith("https//")){
 							port = 443;
@@ -71,31 +77,41 @@ public class Proxy {
 							port = Integer.valueOf(parts[1]).intValue();
 						}
 						host = parts[0];
+					}else if(lineWithoutSpace.startsWith("connection:")){
+						request += line.replace("keep-alive", "close") + "\r\n";
+					}else{
+						request += line + "\r\n";
 					}
 				}
-				System.out.println();
-				System.out.println("host: " + host + " port: " + port + " request type: " + request);
 				
-				if(request.equals("CONNECT")){ // connect
+				if(requestType.equals("CONNECT")){ // connect
 					
 				}else{ // else
+					Socket sender = new Socket(host, port);
+					// send request to web site
+					PrintWriter s_out = new PrintWriter(sender.getOutputStream(), true);
+					s_out.println(request);
 					
+					// read the reply from the web site and forward it to the browser
+					copyStream(sender.getInputStream(), s.getOutputStream());
+					
+					// close the socket
+					sender.close();
+					s.close();
 				}
-				
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-
-			
-			
-			
-			
-			
-			
-			// close the socket!
+		}
+		
+		private static void copyStream(InputStream input, OutputStream output){
+			byte[] buffer = new byte[1024]; // Adjust if you want
+			int bytesRead;
 			try {
-				s.close();
+				while ((bytesRead = input.read(buffer)) != -1){
+					output.write(buffer, 0, bytesRead);
+				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
